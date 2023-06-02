741579b6cf6d23b8dcc4d0094d99db59f47f0795
MSvrX <savreuxmargaux@gmail.com>
2023-06-02 14:59:57 +0200

    add an exercise

---
+++ while1.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// Boucle While, For, Do...While
+
+//Dernière valeur de boucle
+
+//Quelle est la dernière valeur affichée par ce code ? Pourquoi ?
+
+// let i = 3;
+
+// while (i) {
+//   alert(i--);
+// }
+
+// Réponse: La boucle décrémente de un jusqu'à ce que la variable arrive à 0. A 0, la boucle s'arrête car 0 est une valeur fausse.
