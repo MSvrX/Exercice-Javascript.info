b3f4823c1873f7d1758b33493f9499ba5bcb6d45
MSvrX <savreuxmargaux@gmail.com>
2023-06-02 12:38:11 +0200

    complete exercise 1

---
+++ main.js
@@ -0,0 +1,15 @@
+// Boucle While, For, Do...While
+
+//Dernière valeur de boucle
+
+//Quelle est la dernière valeur affichée par ce code ? Pourquoi ?
+
+let i = 3;
+
+while (i) {
+  alert(i--);
+}
+
+// la boucle décrémente de un jusqu'à ce que la variable arrive à 0. A 0, la boucle s'arrête car 0 est une valeur fausse.
+
+//-----------------------------------------------------------------------------------------
