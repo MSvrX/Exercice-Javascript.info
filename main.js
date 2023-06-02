3b40d97c180d6f1a6d85cf4840dc4eccf21106b8
MSvrX <savreuxmargaux@gmail.com>
2023-06-02 12:53:12 +0200

    complete exercise 2

--- main.js
+++ main.js
@@ -4,12 +4,33 @@
 
 //Quelle est la dernière valeur affichée par ce code ? Pourquoi ?
 
-let i = 3;
+// let i = 3;
 
-while (i) {
-  alert(i--);
-}
+// while (i) {
+//   alert(i--);
+// }
 
-// la boucle décrémente de un jusqu'à ce que la variable arrive à 0. A 0, la boucle s'arrête car 0 est une valeur fausse.
+// Réponse: La boucle décrémente de un jusqu'à ce que la variable arrive à 0. A 0, la boucle s'arrête car 0 est une valeur fausse.
+
+//-----------------------------------------------------------------------------------------
+
+//Quelles valeurs affiche la boucle while ?
+
+/*A votre avis, quelles sont les valeurs affichées pour chaque boucle ? Notez-les puis comparer avec la réponse.
+
+Les deux boucles affichent-elles les mêmes valeurs dans l’alert ou pas ?*/
+
+// 1. Le préfixe sous forme ++i :
+
+// let i = 0;
+// while (++i < 5) alert(i);
+
+// 2.Le postfixe sous forme i++ :
+
+// let i = 0;
+// while (i++ < 5) alert(i);
+
+//Réponse: Les deux boucles n'affichent pas les mêmes valeurs car dans la première boucle, l'incrémentation est en préfixe, par conséquent la première valeur renvoyée est la nouvelle valeur modifiée. Dans la deuxième boucle, l'incrémentation est en postfixe, de ce fait, la première valeur renvoyée est donc la dernière valeur non modifiée.
+// Parce que l'alert est une autre instruction de la boucle, la deuxième boucle affichera 5 car il affichera la dernière valeur de i mais dans la comparaison, i affichera 4 et donc 4<5.
 
 //-----------------------------------------------------------------------------------------
